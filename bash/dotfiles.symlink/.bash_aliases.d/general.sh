16010e7897453a4b9e68803a1cc57d6ead77a649
Elliot Boney <elliotboney@gmail.com>
2013-09-05 22:23:38 -0500

    fixed sublime

--- bash/dotfiles.symlink/.bash_aliases.d/general.sh
+++ bash/dotfiles.symlink/.bash_aliases.d/general.sh
@@ -4,4 +4,4 @@ alias mountmainpc='sshfs eboney@mainpc:/ /Users/eboney/mainpc/ -ocache=no -onolo
 # Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
 alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm update npm -g; npm update -g; sudo gem update'
 
-alias mksubl='ln -s /Applications/Sublime Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl'
+alias mksubl='ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl'
