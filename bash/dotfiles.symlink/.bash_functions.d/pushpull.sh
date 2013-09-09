2f48ad5925c51087c6438ecdab3af12bdc2a165b
Elliot Boney <elliotboney@gmail.com>
2013-09-09 10:58:06 -0500

    {}

--- bash/dotfiles.symlink/.bash_functions.d/pushpull.sh
+++ bash/dotfiles.symlink/.bash_functions.d/pushpull.sh
@@ -1,21 +1,22 @@
 function pushimage () {
-  cat $1 | ssh profit "cat > /var/www/images/$1"
+  cat $1 | ssh profit "cat > /var/www/iwillbefamo.us/content/shared/img/$1"
 }
 
 function pushcss () {
-  cat $1 | ssh profit "cat > /var/www/css/$1"
+  cat $1 | ssh profit "cat > /var/www/iwillbefamo.us/content/shared/css/$1"
 }
 
 function pushjs () {
-  cat $1 | ssh profit "cat > /var/www/js/$1"
+  cat $1 | ssh profit "cat > /var/www/iwillbefamo.us/content/shared/js/$1"
 }
 
 function push () {
   if [[ -z "$var" ]]; then
-    echo -e "\n\t${BWhite}Useage: push <local> <remotepath> <sshhost>${NC}"
-    echo -e "\texample: push file.txt /home/user sshhost\n"
+   cat $1 | ssh $3 "cat > $2/$1"
   else 
-    cat $1 | ssh $3 "cat > $2/$1"
+     echo -e "\n\t${BWhite}Useage: push <local> <remotepath> <sshhost>${NC}"
+    echo -e "\texample: push file.txt /home/user sshhost\n"
+    
   fi
 }
 
