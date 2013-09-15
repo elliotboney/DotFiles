51e6286f5702310d91c3aed6c40fc10648526a83
Elliot Boney <elliotboney@gmail.com>
2013-09-15 09:10:07 -0500

    {}

--- bash/dotfiles.symlink/.bash_functions.d/pushpull.sh
+++ bash/dotfiles.symlink/.bash_functions.d/pushpull.sh
@@ -12,16 +12,18 @@ function pushjs () {
 
 function push () {
   if [[ -z "$1" ]]; then
-   cat $1 | ssh $3 "cat > $2/$1"
-  else 
-     echo -e "\n\t${BWhite}Useage: push <local> <remotepath> <sshhost>${NC}"
+    echo -e "\n\t${BWhite}Useage: push <local> <remotepath> <sshhost>${NC}"
     echo -e "\texample: push file.txt /home/user sshhost\n"
+   
+  else 
+    echo $1
+     cat $1 | ssh $3 "cat > $2"
     
   fi
 }
 
 function pull () {
-  if [[ -z "$var" ]]; then
+  if [[ -z "$1" ]]; then
     echo -e "\n\t${BWhite}Useage: pull <sshhost> <remotepath> <local>${NC}"
     echo -e "\texample: pull sshhost /var/www/index.php ~/file.txt \n"
   else 
