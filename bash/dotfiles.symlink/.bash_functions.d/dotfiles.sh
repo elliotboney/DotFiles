250c550adb4b9b0580c00877766e23bef1392263
Elliot Boney <elliotboney@gmail.com>
2013-09-30 10:46:14 -0500

    {}

--- bash/dotfiles.symlink/.bash_functions.d/dotfiles.sh
+++ bash/dotfiles.symlink/.bash_functions.d/dotfiles.sh
@@ -1,11 +1,6 @@
 function pushdots()   # Get current host related info.
 {
-    echo -e "\nMoving to DotFiles"
-    cd ~/Dropbox/DotFiles
-    git add -A .
-    git commit -m "{$1}"
-    git push
-    cl
+    cd ~/Dropbox/DotFiles && git add -A . && git commit -m "{$1}" && git push && cd -
 }
 
 function updatedotfiles() {
