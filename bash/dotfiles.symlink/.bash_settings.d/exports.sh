80170d06168ccb433fb3b5309bf21b807d51f28a
Elliot Boney <elliotboney@gmail.com>
2013-10-04 15:54:26 -0500

    {}

--- bash/dotfiles.symlink/.bash_settings.d/exports.sh
+++ bash/dotfiles.symlink/.bash_settings.d/exports.sh
@@ -22,4 +22,13 @@ GREP_COLORS=${GREP_COLORS}:"ln=32:"
 GREP_COLORS=${GREP_COLORS}:"bn=38;5;238:"
 GREP_COLORS=${GREP_COLORS}:"se=38;5;238:"
 # Use home for ruby gems
-#export GEM_HOME="~/.gem"
+export GEM_HOME="~/.gem"
+
+
+# XDG Base Directory Specification
+# http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export ZSH_CONFIG="$XDG_CONFIG_HOME/zsh"
+export ZSH_CACHE="$XDG_CACHE_HOME/zsh"
+mkdir -p $ZSH_CACHE
