59a6c3d8b50dbf0bac0988b6fe60bebbd947e2fa
Elliot Boney <elliotboney@gmail.com>
2013-10-09 20:11:28 -0500

    added updateallgit()

--- bash/dotfiles.symlink/.bash_functions.d/git.sh
+++ bash/dotfiles.symlink/.bash_functions.d/git.sh
@@ -3,3 +3,13 @@ function tester()
   DIR=$(python -c "import os; print os.path.realpath(\"${1}\")")
   echo $DIR
 }
+
+function updateallgit()
+{
+    if [[ -z "$1" ]]; then
+        echo -e "\n\t${BCyan} Useage: ${White}updateallgit ${Yellow}<${LightGray}directory${Yellow}>\n"
+    else
+        cd $1; ls | parallel 'cd {} && git pull'
+    fi
+
+}
