cba325b15a323869a0bcdfef29fdca3437792395
Elliot Boney <elliotboney@gmail.com>
2013-10-12 23:24:36 -0500

    {}

--- bash/dotfiles.symlink/.bash_functions.d/extract.sh
+++ bash/dotfiles.symlink/.bash_functions.d/extract.sh
@@ -30,7 +30,7 @@ extract () {
 function maketar() { tar cvzf "${1%%/}.tar.gz"  "${1%%/}/"; }
 
 # Create a ZIP archive of a file or folder.
-function makezip() {  "${1%%/}.zip" "$1" ; }
+function makezip() { zip -r "${1%%/}.zip" "$1" ; }
 
 #tar.gz
 tg() {
