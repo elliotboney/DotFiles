2b6df24c217be900e6e631e974cab0c134410fd8
Elliot Boney <elliotboney@gmail.com>
2014-02-05 14:37:05 -0600

    Auto dotfiles push -- 02_05_2014
    Former-commit-id: d822f760ab03642c4ca19ecd16d3042a5bc35c66

--- bash/dotfiles.symlink/.bash_functions.d/dotfiles.sh
+++ bash/dotfiles.symlink/.bash_functions.d/dotfiles.sh
@@ -12,3 +12,20 @@ function pushdots()   # Get current host related info.
 function updatedotfiles() {
     cd ${DOTPATH} && git reset --hard origin/master && git pull && cd - && source $HOME/.zshrc
 }
+
+function lm () {
+  MVFILE=$1
+  MVFILE=${MVFILE:1:${#MVFILE}}
+  # return
+   # ${DIR:1:${#DIR}}
+ if [[ -z "$1" ]]; then
+        # echo a help message if no port is specified
+        echo -e "\n\t${White}Useage: ${BCyan}lm ${LightGray}<file> ${NC}\n"
+  else
+      # cd ${DOTPATH} && git add -A . && git commit -m "$1" && git push && cd -
+      mv "$1" ~/Git/DotFiles/misc/${MVFILE}.symlink
+      ln -s ~/Git/DotFiles/misc/${MVFILE}.symlink ~/.${MVFILE}
+    echo -e "\n\t${White}Moved and created a symlink for: ${BCyan}.${MVFILE}${NC}\n"
+
+  fi
+}
