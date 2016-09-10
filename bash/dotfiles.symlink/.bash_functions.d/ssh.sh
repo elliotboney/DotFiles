7f6cb6e3f66fd5b9a6ea716ea0dd7f8fe3e607b6
Elliot Boney <elliotboney@gmail.com>
2016-09-10 12:58:40 -0500

    Auto dotfiles push -- 09_10_2016

--- bash/dotfiles.symlink/.bash_functions.d/ssh.sh
+++ bash/dotfiles.symlink/.bash_functions.d/ssh.sh
@@ -11,26 +11,26 @@ function sshkey {
 }
 
 ######################## For SSH Remote Copy #########################
-export LC_SETUP_RC='command -v rclip >/dev/null 2>&1 || { echo "executing"; mkdir -p /usr/local/bin; if [ ! -f /usr/local/bin/rclip ];then wget https://raw.githubusercontent.com/justone/remotecopy/master/remotecopy -P /usr/local/bin/; ln -s /usr/local/bin/remotecopy /usr/local/bin/rclip; chmod +x /usr/local/bin/remotecopy; fi; if [[ \":\$PATH:\" == *\"/usr/local/bin:\"* ]]; then export PATH=/usr/local/bin:$PATH; fi } > /var/log/rclip.log 2>&1 || echo "Some error occured in setting up rclip. check /var/log/rclip.log"'
+# export LC_SETUP_RC='command -v rclip >/dev/null 2>&1 || { echo "executing"; mkdir -p /usr/local/bin; if [ ! -f /usr/local/bin/rclip ];then wget https://raw.githubusercontent.com/justone/remotecopy/master/remotecopy -P /usr/local/bin/; ln -s /usr/local/bin/remotecopy /usr/local/bin/rclip; chmod +x /usr/local/bin/remotecopy; fi; if [[ \":\$PATH:\" == *\"/usr/local/bin:\"* ]]; then export PATH=/usr/local/bin:$PATH; fi } > /var/log/rclip.log 2>&1 || echo "Some error occured in setting up rclip. check /var/log/rclip.log"'
 
-ssh_function() {
-  count="`ps -eaf | grep remotecopyserver | grep -v grep | wc -l`";
-  if [ "$count" -eq "0" ]; then
-   mkdir -p $HOME/bin;
-   if [ ! -f $HOME/bin/remotecopyserver ]; then
-    wget https://raw.githubusercontent.com/justone/remotecopy/master/remotecopyserver -P $HOME/bin;
-    chmod +x $HOME/bin/remotecopyserver;
-  fi;
-  nohup $HOME/bin/remotecopyserver &
-fi;
-ssh_cmd=`which ssh`
-PARAMS=""
-for PARAM in "$@"
-do
-  PARAMS="${PARAMS} \"${PARAM}\""
-done
-bash -c "ssh ${PARAMS} -R 12345:localhost:12345 -t 'echo \$LC_SETUP_RC | sudo bash; bash -l'"
-}
-alias ssho=`which ssh`
-alias ssh=ssh_function
-alias ssh2=ssh_function
+# ssh_function() {
+#   count="`ps -eaf | grep remotecopyserver | grep -v grep | wc -l`";
+#   if [ "$count" -eq "0" ]; then
+#    mkdir -p $HOME/bin;
+#    if [ ! -f $HOME/bin/remotecopyserver ]; then
+#     wget https://raw.githubusercontent.com/justone/remotecopy/master/remotecopyserver -P $HOME/bin;
+#     chmod +x $HOME/bin/remotecopyserver;
+#   fi;
+#   nohup $HOME/bin/remotecopyserver &
+# fi;
+# ssh_cmd=`which ssh`
+# PARAMS=""
+# for PARAM in "$@"
+# do
+#   PARAMS="${PARAMS} \"${PARAM}\""
+# done
+# bash -c "ssh ${PARAMS} -R 12345:localhost:12345 -t 'echo \$LC_SETUP_RC | sudo bash; bash -l'"
+# }
+# alias ssho=`which ssh`
+# alias ssh=ssh_function
+# alias ssh2=ssh_function
