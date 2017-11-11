3b8b30d5fd2a70332a561087a531aa11d5e92fe7
Elliot Boney <elliotboney@gmail.com>
2017-11-11 07:26:36 -0600

    alias

---
+++ bash/dotfiles.symlink/.bash_aliases.d/development.sh
@@ -0,0 +1,20 @@
+#########################
+#          SSH          #
+#########################
+alias prod="ssh eboney@173.255.203.251 -p 2222"
+alias ell='_changetitle elliotboney.com && ssh elliotboney.com'
+
+
+#########################
+#          PHP          #
+#########################
+
+alias createdocs='phpdoc -d ./library/ZFDebug -t docs --template clean'
+
+
+#########################
+#         MISC          #
+#########################
+
+# Outputs shell colors
+alias colortest='for x in 0 1 4 5 7 8; do for i in `seq 30 37`; do for a in `seq 40 47`; do echo -ne "\e[$x;$i;$a""m\\\x1b[$x;$i;$a""m\e[0;37;40m "; done; echo; done; done; echo "";'
--- bash/dotfiles.symlink/.bash_aliases.d/general.sh
+++ bash/dotfiles.symlink/.bash_aliases.d/general.sh
@@ -1,3 +1,7 @@
+# alias mountmainpc='sshfs eboney@mainpc:/ /Users/eboney/mainpc/ -ocache=no -onolocalcaches -ovolname=mainpc'
+
+
+
 alias cleanscreen="screen -ls | cut -d. -f1 | awk '{print $1}' | awk '{print $1}'| xargs -I{} screen -S {} -X quit"
 # Find running task, case insensitive search
 alias pg='ps aux | head -n1; ps aux | grep -i'
@@ -8,11 +12,6 @@ alias castlivingroom='castnow --device "Dobbie"'
 
 alias -g TOPNG='| convert label:@-.png'
 
-# unalias c
-
-alias adl='/Applications/Adobe\ Flash\ CC\ 2015/AIR17.0/bin/adl'
-alias adt='/Applications/Adobe\ Flash\ CC\ 2015/AIR17.0/bin/adt'
-
 alias fuck='eval $(thefuck $(fc -ln -1))'
 # You can use whatever you want as an alias, like for Mondays:
 alias FUCK='fuck'
@@ -22,24 +21,15 @@ alias FUCK='fuck'
 alias kextlist="kextstat -kl | awk '{printf \"%i %i %s %s\n\", \$4 / 1024, \$5 / 1024, \$6, \$7}' | sort -nr"
 
 # I never remember to sudo
-alias service='sudo service'
 alias apachectl='sudo apachectl'
 alias a2endmod='sudo a2endmod'
 alias a2dismod='sudo a2dismod'
 alias a2ensite='sudo a2ensite'
 alias a2dissite='sudo a2dissite'
+alias apt='sudo apt'
+alias aptitude='sudo aptitude'
+alias apt-get='sudo apt-get'
 
-# Outputs shell colors
-alias colortest='for x in 0 1 4 5 7 8; do for i in `seq 30 37`; do for a in `seq 40 47`; do echo -ne "\e[$x;$i;$a""m\\\x1b[$x;$i;$a""m\e[0;37;40m "; done; echo; done; done; echo "";'
-
-# if shell_is_elliot; then
-alias createdocs='phpdoc -d ./library/ZFDebug -t docs --template clean'
-# alias mountmainpc='sshfs eboney@mainpc:/ /Users/eboney/mainpc/ -ocache=no -onolocalcaches -ovolname=mainpc'
-# fi
-
-if shell_is_linux; then
-    alias service='sudo service'
-fi
 
 alias updatefonts='sudo fc-cache -f -v'
 # command_exists brew ] && echo "true"
