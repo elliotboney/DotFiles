6d412332371066c8e8fdefe822281a938a5b6f88
Elliot Boney <elliotboney@gmail.com>
2017-11-23 12:04:04 -0600

    fixed

--- setup_brew.sh
+++ setup_brew.sh
@@ -1,2 +1,16 @@
-brew tap homebrew/boneyard
+#!/bin/bash
+#
+# Install all the things with Homebrew, Casks and a Brewfile
+#
+
+# If Homebrew is not installed
+if ! which brew > /dev/null; then
+     # Install Homebrew
+     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi;
+
+# Update brew
+brew update
+
+# Install everything in Brewfile
 brew bundle
