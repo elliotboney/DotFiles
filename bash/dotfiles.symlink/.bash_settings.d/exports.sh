7e028c7f98f359e5a2617b4bad643259f863c7bf
Elliot Boney <elliotboney@gmail.com>
2018-07-23 12:56:10 -0500

    fix

--- bash/dotfiles.symlink/.bash_settings.d/exports.sh
+++ bash/dotfiles.symlink/.bash_settings.d/exports.sh
@@ -3,14 +3,13 @@
 #
 if [ -z ${ZSH_HIGHLIGHT_STYLES+x} ]; then
   # zsh highlight stuff isn't set
-  echo -e "You should run 'sudo aptititude install zsh-syntax-highlighting' to install ZSH highlighting"
-  else
-  export ZSH_HIGHLIGHT_STYLES[path]='fg=33,underline'
-  export ZSH_HIGHLIGHT_STYLES[path_pathseperator]='fg=33,underline,bold'
-  # command separation tokens (;, &&)
-  export ZSH_HIGHLIGHT_STYLES[commandseparator]='fg=bold'
-  export ZSH_HIGHLIGHT_STYLES[globbing]='fg=cyan'
+  declare -a ZSH_HIGHLIGHT_STYLES
 fi
+export ZSH_HIGHLIGHT_STYLES[path]='fg=33,underline'
+export ZSH_HIGHLIGHT_STYLES[path_pathseperator]='fg=33,underline,bold'
+# command separation tokens (;, &&)
+export ZSH_HIGHLIGHT_STYLES[commandseparator]='fg=bold'
+export ZSH_HIGHLIGHT_STYLES[globbing]='fg=cyan'
 # rando
 export GIT_EXECUTABLE=$(which git)
 
@@ -30,7 +29,7 @@ export DEVKITPPC="${DEVKITPRO}//devkitPPC"
 export PYENV_ROOT=/usr/local/var/pyenv
 if command_exists pyenv; then eval "$(pyenv init -)"; fi
 
-# ignore annoying https warnings
+  # ignore annoying https warnings
 PYTHONWARNINGS="ignore:Unverified HTTPS request"
 
 
@@ -38,62 +37,62 @@ PYTHONWARNINGS="ignore:Unverified HTTPS request"
 export NVM_SYMLINK_CURRENT=true
 
 # if command_exists vimpager; then
-#   export PAGER=/usr/local/bin/vimpager
-#   export VIMPAGER_RC=${HOME}/.vimpagerrc
-#   alias less="$PAGER -c \"set number\""
-#   alias zless=$PAGER
-# fi
+  #   export PAGER=/usr/local/bin/vimpager
+  #   export VIMPAGER_RC=${HOME}/.vimpagerrc
+  #   alias less="$PAGER -c \"set number\""
+  #   alias zless=$PAGER
+  # fi
 
-export NVM_DIR="${HOME}/.nvm"
+  export NVM_DIR="${HOME}/.nvm"
 
-#export for Atom Editor
-export ATOM_REPOS_HOME=${HOME}/Code/Atom
+  #export for Atom Editor
+  export ATOM_REPOS_HOME=${HOME}/Code/Atom
 
-# Link Homebrew casks in `/Applications` rather than `${HOME}/Applications`
-export HOMEBREW_CASK_OPTS="--appdir=/Applications";
-
-
-# For brew autocompletion
-export HOMEBREW_SEARCH_CACHE_PATH=${HOME}/.homebrew-search-cache
-
-export TERM=xterm-256color
-
-# Less Colors for Man Pages
-export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
-export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
-export LESS_TERMCAP_me=$'\E[0m'           # end mode
-export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
-export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
-export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
-export LESS_TERMCAP_ue=$'\E[0m'           # end underline
-
-export XML_CATALOG_FILES="/usr/local/etc/xml/catalog"
-
-# Always enable colored `grep` output
-# export GREP_OPTIONS="--color=auto"
-
-# Prefer US English and use UTF-8
-export LANG="en_US"
-export LC_ALL="en_US.UTF-8"
-
-# Make some commands not show up in history
-export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"
-
-GREP_COLORS="ms=48;5;199;38;5;255:"
-GREP_COLORS=${GREP_COLORS}:"mc=01;33:"
-GREP_COLORS=${GREP_COLORS}:"sl=38;5;245:"
-GREP_COLORS=${GREP_COLORS}:"cx=38;5;238:"
-GREP_COLORS=${GREP_COLORS}:"fn=31:"
-GREP_COLORS=${GREP_COLORS}:"ln=32:"
-GREP_COLORS=${GREP_COLORS}:"bn=38;5;238:"
-export GREP_COLORS=${GREP_COLORS}:"se=38;5;238:"
+  # Link Homebrew casks in `/Applications` rather than `${HOME}/Applications`
+  export HOMEBREW_CASK_OPTS="--appdir=/Applications";
 
 
+  # For brew autocompletion
+  export HOMEBREW_SEARCH_CACHE_PATH=${HOME}/.homebrew-search-cache
 
-# XDG Base Directory Specification
-# http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_CACHE_HOME="$HOME/.cache"
-export ZSH_CONFIG="$XDG_CONFIG_HOME/zsh"
-export ZSH_CACHE="$XDG_CACHE_HOME/zsh"
-mkdir -p $ZSH_CACHE
+  export TERM=xterm-256color
+
+  # Less Colors for Man Pages
+  export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
+  export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
+  export LESS_TERMCAP_me=$'\E[0m'           # end mode
+  export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
+  export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
+  export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
+  export LESS_TERMCAP_ue=$'\E[0m'           # end underline
+
+  export XML_CATALOG_FILES="/usr/local/etc/xml/catalog"
+
+  # Always enable colored `grep` output
+  # export GREP_OPTIONS="--color=auto"
+
+  # Prefer US English and use UTF-8
+  export LANG="en_US"
+  export LC_ALL="en_US.UTF-8"
+
+  # Make some commands not show up in history
+  export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"
+
+  GREP_COLORS="ms=48;5;199;38;5;255:"
+  GREP_COLORS=${GREP_COLORS}:"mc=01;33:"
+  GREP_COLORS=${GREP_COLORS}:"sl=38;5;245:"
+  GREP_COLORS=${GREP_COLORS}:"cx=38;5;238:"
+  GREP_COLORS=${GREP_COLORS}:"fn=31:"
+  GREP_COLORS=${GREP_COLORS}:"ln=32:"
+  GREP_COLORS=${GREP_COLORS}:"bn=38;5;238:"
+  export GREP_COLORS=${GREP_COLORS}:"se=38;5;238:"
+
+
+
+  # XDG Base Directory Specification
+  # http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+  export XDG_CONFIG_HOME="$HOME/.config"
+  export XDG_CACHE_HOME="$HOME/.cache"
+  export ZSH_CONFIG="$XDG_CONFIG_HOME/zsh"
+  export ZSH_CACHE="$XDG_CACHE_HOME/zsh"
+  mkdir -p $ZSH_CACHE
